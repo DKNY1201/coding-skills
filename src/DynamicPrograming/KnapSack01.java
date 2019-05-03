b040d3cfe2f77c87255194c283d3996739565e96
Quy Tran <quytran288@gmail.com>
2019-05-02 21:27:47 -0700

    0/1 Knapsack

---
+++ src/DynamicPrograming/KnapSack01.java
@@ -0,0 +1,29 @@
+package DynamicPrograming;
+
+/**
+ * Created by Bi on 5/2/19.
+ */
+public class KnapSack01 {
+    public static void main(String[] args) {
+        int res = knapSack01(new int[] {5, 4, 6, 3}, new int[] {10, 40, 30, 50}, 10);
+        int res1 = knapSack01(new int[] {1, 3, 4, 5}, new int[] {1, 4, 5, 7}, 7);
+        System.out.println(res);
+        System.out.println(res1);
+    }
+
+    public static int knapSack01(int[] w, int[] v, int total) {
+        int[][] table = new int[w.length + 1][total + 1];
+
+        for (int i = 1; i <= w.length; i++) {
+            for (int j = 1; j <= total; j++) {
+                if (w[i - 1] > j) {
+                    table[i][j] = table[i - 1][j];
+                } else {
+                    table[i][j] = Math.max(v[i - 1] + table[i - 1][j - w[i - 1]], table[i - 1][j]);
+                }
+            }
+        }
+
+        return table[w.length][total];
+    }
+}
