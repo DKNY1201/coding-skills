313ed3dcae97c629aba8ca307376a985dd823041
Quy Tran <quytran288@gmail.com>
2019-05-03 23:33:21 -0700

    Implementing Recursive and Memoize verson of KnapSack01 problem

--- src/DynamicPrograming/KnapSack01.java
+++ src/DynamicPrograming/KnapSack01.java
@@ -1,5 +1,7 @@
 package DynamicPrograming;
 
+import java.util.HashMap;
+
 /**
  * Created by Bi on 5/2/19.
  */
@@ -12,6 +14,50 @@ public class KnapSack01 {
     }
 
     public static int knapSack01(int[] w, int[] v, int total) {
+//        return recursive(w, v, total, w.length - 1);
+        return memoize(w, v, total, w.length - 1, new HashMap<String, Integer>());
+//        return bottomUp(w, v, total);
+    }
+
+    public static int recursive(int[] w, int[] v, int total, int i) {
+        // Base case
+        if (i == 0) {
+            if (total < w[i]) return 0;
+            return v[i];
+        }
+
+        // Recursive case
+        if (total < w[i]) return recursive(w, v, total, i - 1);
+
+        return Math.max(recursive(w, v, total, i - 1), v[i] + recursive(w, v, total - w[i], i - 1));
+    }
+
+    public static int memoize(int[] w, int[] v, int total, int i, HashMap<String, Integer> map) {
+        String key = total + ":" + i;
+        if (map.containsKey(key)) {
+            return map.get(key);
+        }
+
+        // Base case
+        if (i == 0) {
+            if (total < w[i]) return 0;
+            return v[i];
+        }
+
+        // Recursive case
+        int res;
+
+        if (total < w[i]){
+            res = recursive(w, v, total, i - 1);
+        }
+
+        res = Math.max(recursive(w, v, total, i - 1), v[i] + recursive(w, v, total - w[i], i - 1));
+        map.put(key, res);
+
+        return res;
+    }
+
+    public static int bottomUp(int[] w, int[] v, int total) {
         int[][] table = new int[w.length + 1][total + 1];
 
         for (int i = 1; i <= w.length; i++) {
