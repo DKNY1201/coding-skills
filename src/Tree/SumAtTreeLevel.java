8f11ea1991ee0fdf6efd18ca9b9157b84c46570b
Quy Tran <quytran288@gmail.com>
2019-06-11 17:16:26 -0700

    Sum Tree Level problem iterative

--- src/Tree/SumAtTreeLevel.java
+++ src/Tree/SumAtTreeLevel.java
@@ -1,9 +1,11 @@
 package Tree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Created by Bi on 6/11/19.
  * https://start.interviewing.io/interview/0LTTr0v5NKrD/replay
- *
  */
 public class SumAtTreeLevel {
     public static void main(String[] args) {
@@ -27,13 +29,49 @@ public class SumAtTreeLevel {
         node5.left = node8;
 
         System.out.println(sumAtTreeLevel(root, 3, 1));
+        System.out.println(sumAtTreeLevelIter(root, 4));
     }
 
+    /**
+     * Recursive solution
+     *
+     * @param root
+     * @param lvl
+     * @param curLvl
+     * @return
+     */
     public static int sumAtTreeLevel(TreeNode root, int lvl, int curLvl) {
         if (root == null) return 0;
         if (curLvl == lvl) return root.val;
 
-        return sumAtTreeLevel(root.left,lvl,curLvl+1) + sumAtTreeLevel(root.right,lvl,curLvl+1);
+        return sumAtTreeLevel(root.left, lvl, curLvl + 1) + sumAtTreeLevel(root.right, lvl, curLvl + 1);
+    }
+
+    public static int sumAtTreeLevelIter(TreeNode root, int lvl) {
+        root.lvl = 1;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int total = 0;
+
+        while (!queue.isEmpty()) {
+            TreeNode curNode = queue.remove();
+
+            if (curNode.lvl == lvl) {
+                total += curNode.val;
+            } else if (curNode.lvl < lvl) {
+                if (curNode.left != null) {
+                    curNode.left.lvl = curNode.lvl + 1;
+                    queue.add(curNode.left);
+                }
+
+                if (curNode.right != null) {
+                    curNode.right.lvl = curNode.lvl + 1;
+                    queue.add(curNode.right);
+                }
+            }
+        }
+
+        return total;
     }
 }
 
@@ -41,6 +79,7 @@ class TreeNode {
     public int val;
     public TreeNode left;
     public TreeNode right;
+    public int lvl;
 
     public TreeNode(int val) {
         this.val = val;
